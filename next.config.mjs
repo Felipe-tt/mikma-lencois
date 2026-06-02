c5e0d4815ef3662fda72064ee4cece3523a38a5e
Felipe-tt <felipeittnerc@gmail.com>
2026-06-02 13:11:49 +0000

    remove paginas duplicadas

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['@node-rs/argon2'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
