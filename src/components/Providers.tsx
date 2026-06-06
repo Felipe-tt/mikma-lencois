06ea57c57f0cef4292ed32397928c6e693b867ff
Felipe-tt <felipeittnerc@gmail.com>
2026-06-06 13:07:13 +0000

    remove TopProgress

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import { AuthProvider } from '@/lib/auth/AuthContext';
-import { TopProgress } from '@/components/ui/TopProgress';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <TopProgress />
       {children}
     </AuthProvider>
   );
